c9b526a29a388a63ed9e31521562964e8a10558d
LElTEDEV <zinenizi@gmail.com>
2025-11-22 11:04:37 -0300

    feat: created countdown component

---
+++ src/components/CountDown.tsx
@@ -0,0 +1,7 @@
+export function CountDown() {
+  return (
+    <div className="flex items-center justify-center">
+      <span className="text-8xl md:text-9xl font-bold">00:00</span>
+    </div>
+  );
+}
