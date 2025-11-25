f250dd7334fdec66851c57b7a0bd99164b4f4a9e
LElTEDEV <zinenizi@gmail.com>
2025-11-25 13:35:01 -0300

    feat: created status props

--- src/components/DefaultButton.tsx
+++ src/components/DefaultButton.tsx
@@ -1,25 +1,28 @@
 import type { ComponentProps } from "react";
+import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
 
 type DefaultButtonProps = ComponentProps<"button"> & {
-  icon: React.ReactNode;
-  color?: "green" | "red";
+  status?: "play" | "stop";
 };
 
 export function DefaultButton({
-  icon,
-  color = "green",
+  status = "play",
   ...rest
 }: DefaultButtonProps) {
   return (
     <button
       className={`flex items-center justify-center min-w-88 p-3 rounded cursor-pointer my-12 hover:brightness-150 transition ease-in ${
-        color === "green"
+        status === "play"
           ? "bg-primary text-text-over-primary"
           : "bg-error text-text-over-error"
       }`}
       {...rest}
     >
-      {icon}
+      {status === "play" ? (
+        <PlayCircleIcon size={38} />
+      ) : (
+        <StopCircleIcon size={38} />
+      )}
     </button>
   );
 }
