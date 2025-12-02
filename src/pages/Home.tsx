b6f6d9161a2e54f619c8a1f1f62c3ee35cb95fbb
LElTEDEV <zinenizi@gmail.com>
2025-12-02 14:42:39 -0300

    feat: created home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import { MainForm } from "../components/MainForm";
+import { Container } from "../components/Container";
+import { CountDown } from "../components/CountDown";
+import { MainTemplate } from "../templates/MainTemplate";
+
+export function Home() {
+  return (
+    <MainTemplate>
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <MainForm />
+      </Container>
+    </MainTemplate>
+  );
+}
